fa3295df74bd5ec4f5b9681678f60929d949abed
Thomas Ruppeiner <truppeiner@gmail.com>
2022-08-23 15:23:36 -0500

    authentication added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,20 @@
 const { User } = require('../models');
+const { AuthenticationError} = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        // get logged in user
+        me: async (parent, args, context) => {
+            if (context.user){
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('__v -password')
+
+                return userData
+            }
+            throw new AuthenticationError('Not logged in!');
+        },
+        
         // get all users
         users: async () => {
             return User.find()
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,11 +3,13 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
     type User {
         _id: ID
-        username: String 
+        username: String
+        email: String 
         }
 
     type Query {
         users: [User]
+        me: User
     }
 
 `
