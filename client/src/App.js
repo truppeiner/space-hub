fa737b8f7d385bbfa7a480d4b8c17dadc94d0cae
Thomas Ruppeiner <truppeiner@gmail.com>
2022-08-25 19:04:12 -0500

    connected front-end to back-end login/logout tested

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,47 @@
 import Header from './components/layout/Header';
 
+// ApolloClient Link functionality 
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import { setContext } from '@apollo/client/link/context';
+
+// if client runs into error run script
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      return alert(`graphQL errpr ${message}`);
+    })
+  }
+});
+
+// establish token grab
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// establish link to backend
+const link = from ([
+  errorLink,
+  new HttpLink({ uri: "http://localhost:3001/graphql"}),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(link)
+})
 function App() {
   return (
+  <ApolloProvider client = {client}>
     <div className="App">
       <Header/>
     </div>
+  </ApolloProvider>
   );
 }
 
--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,12 +1,106 @@
+import React, {useState} from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Auth from '../../utils/auth';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../../utils/mutations';
 
-function Header(){
+// import { GiSpaceShuttle } from 'react-icons/gi';
+
+function Header( props ){
+    // set form state
+    const [formState, setFormState] = useState({ email: "", password: ""});
+    const [login] = useMutation(LOGIN_USER);
+
+    // function to logout
+    const logout = event => {
+        event.preventDefault();
+        Auth.logout();
+    };
+
+    // function to handle login modal
+    const [show, setShow] = useState(false);
+    
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    // function to update modal form based on input 
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        })
+    };
+
+    // handle login form submit
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const { data } = await login({
+                variables: { ...formState },
+            });
+            Auth.login(data.login.token);
+        } catch (error){
+            console.log(error);
+        }
+
+        // clear form values
+        setFormState({
+            email: "",
+            password: "",
+        });
+    };
+
     return(
         <>
             <Navbar bg='light'>
                 <Container>
-                    <p>test</p>
+                    <Navbar.Brand>
+                        <h2>Space Hub</h2>
+                    </Navbar.Brand>
+                    {Auth.loggedIn() ? (
+                        // if logged in render this section
+                            <Navbar.Text>
+                                <Button variant='secondary' onClick={logout}>Logout</Button>
+                            </Navbar.Text>
+                    ): (
+                        // if logged out render this section
+                        <>
+                            <Navbar.Text>
+                                <Button variant='secondary' onClick= {handleShow}>Login</Button>
+                            </Navbar.Text>
+                            {/* will render login modal on button click */}
+                            <Modal show = {show} onHide = {handleClose} animation = { true }>
+                                <Modal.Header>
+                                    Login
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <form onSubmit={handleFormSubmit}>
+                                        <input
+                                            placeholder = 'Enter Email'
+                                            name = 'email'
+                                            type = 'email'
+                                            value = {formState.email}
+                                            onChange = {handleChange}
+                                        />
+                                        <input
+                                            placeholder = 'Enter Password'
+                                            name = 'password'
+                                            type = 'password'
+                                            value = {formState.password}
+                                            onChange = {handleChange}
+                                        />
+                                    <Button variant="secondary" onClick = {handleClose} type="submit">Submit</Button>
+                                    </form>
+                                </Modal.Body>
+                            </Modal>
+                        </>
+                    )}
                 </Container>
             </Navbar>
         </>
